578b7ab5a2c22c21917dd7f37a82f3935c4ebc71
Andrew Douglass <andrew@dou.glass>
2016-03-30 14:15:55 -0500

    Reprojections of shapefiles to wgs84 and import into database.

--- after.sh
+++ after.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # Install and enable PostGIS
-sudo apt-get -y install postgresql-9.4-postgis-2.1
+sudo apt-get -y install postgis postgresql-9.4-postgis-2.1
 export PGHOST="localhost"
 export PGUSER="homestead"
 export PGPASSWORD="secret"
 psql -c "CREATE EXTENSION postgis;"
+
+# Load Harris County Shapefile
+shp2pgsql -s EPSG:4326 -d dev/Shapefiles/2016_Harris_County_VPCTS_v1.2/2016_HarrisCounty_VotingPrecincts_VER1.2_wgs84.shp precincts | psql
