0492d6ca9b52c033ef65d9c56841ce145f2ed435
Andrew Douglass <andrew@dou.glass>
2016-04-19 15:14:33 -0500

    Updated shapefile for Harris County.

--- after.sh
+++ after.sh
@@ -8,4 +8,4 @@ export PGPASSWORD="secret"
 psql -c "CREATE EXTENSION postgis;"
 
 # Load Harris County Shapefile
-shp2pgsql -s EPSG:4326 -d dev/Shapefiles/2016_Harris_County_VPCTS_v1.2/2016_HarrisCounty_VotingPrecincts_VER1.2_wgs84.shp precincts | psql
+shp2pgsql -s EPSG:4326 -d dev/Shapefiles/2016_Harris_County_VPCTS_v1.3/Harris_2016_v1_3.shp precincts | psql
